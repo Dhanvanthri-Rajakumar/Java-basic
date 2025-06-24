d24d99e2b441ed59fb74fe2e6249ef86201bf80f
Dhanvanthri-Rajakumar <anthridhanv7@gmail.com>
2025-06-24 09:38:26 +0530

    Add files via upload

--- array/array.java
+++ array/array.java
@@ -1,1 +1,8 @@
+// print the element in the 0th index
 
+public class array{
+    public static void main(String[] args) {
+        int[] arr = {3,4,5,6,7,8,9,0};
+        System.out.println(arr[0]);
+    }
+}
